b0d1bb518ff7615eb5d41d688953523e29b6333b
Rayan <yayaygrz@gmail.com>
2023-10-25 15:56:58 +0100

    respnsive

--- script.js
+++ script.js
@@ -71,15 +71,15 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 var len = acc.length;
 for (i = 0; i < len; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
 }
 
 // nav bar
